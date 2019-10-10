871c1288bc71bca09bc737cbc7d5e9539d1ef55c
Daniel Mariano Varela <danielmvarela@hotmail.com>
2019-10-10 16:30:41 -0300

    Modificacion

--- FrontEnd/Interface.java
+++ FrontEnd/Interface.java
@@ -1,18 +1,16 @@
 package FrontEnd;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.FlowLayout;
 import javax.swing.JTextField;
-import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JComboBox;
+import javax.swing.border.EmptyBorder;
 
 public class Interface extends JFrame {
 
